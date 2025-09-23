3180e8d0e6b60bd356375d210137905026b6ce52
gustavoweb <contato@gustavoweb.com>
2025-09-23 08:13:32 -0300

    Add files via upload

---
+++ 3-tempo.py
@@ -0,0 +1,38 @@
+import requests
+from datetime import datetime
+
+def get_previsao(lat, lon):
+    url = (
+        "https://api.open-meteo.com/v1/forecast"
+        f"?latitude={lat}&longitude={lon}"
+        "&hourly=temperature_2m,precipitation,windspeed_10m"
+        "&current_weather=true"
+        "&timezone=auto"
+    )
+
+    resposta = requests.get(url)
+    if resposta.status_code == 200:
+        dados = resposta.json()
+
+        # Clima atual
+        tempo_atual = dados.get("current_weather", {})
+        temp_atual = tempo_atual.get("temperature")
+        vento_atual = tempo_atual.get("windspeed")
+        hora_atual = tempo_atual.get("time")
+
+
+        print("=== TEMPO AGORA ===")
+        print(f"Hora: {hora_atual}")
+        print(f"Temperatura: {temp_atual} °C")
+        print(f"Vento: {vento_atual} km/h")
+        print()
+
+  
+    else:
+        print("Erro ao buscar previsão:", resposta.status_code)
+
+if __name__ == "__main__":
+    # Coordenadas de Campinas, SP
+    latitude = -22.9056
+    longitude = -47.0608
+    get_previsao(latitude, longitude)
